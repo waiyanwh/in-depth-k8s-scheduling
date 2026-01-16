d9a3af61c4ef567ffb94c997bd941da408835500
waiyanwh <waiyan@opn.ooo>
2026-01-16 16:05:09 +0700

    feat: final commits

--- 02-taints/cleanup.sh
+++ 02-taints/cleanup.sh
@@ -6,10 +6,7 @@ echo "Cleaning up Module 02 (Taints)..."
 # Delete deployments
 kubectl delete deployment web-app --ignore-not-found
 kubectl delete deployment sec-monitor --ignore-not-found
-
-# Remove production node taints (tier=secure:NoSchedule)
-echo "Removing production node taints..."
-kubectl taint nodes -l env=production tier- 2>/dev/null || true
+kubectl delete -f ../01-affinity/data-processor-flexible.yaml
 
 # Remove maintenance taints (outage=true:NoExecute)
 echo "Removing maintenance taints from all nodes..."
